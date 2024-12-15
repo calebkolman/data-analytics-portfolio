012673f6be0600cb94566fdd37b923afaef5adf2
calebkolman <112969064+calebkolman@users.noreply.github.com>
2024-12-14 21:44:34 -0500

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('movingBtn');
 
-  button.addEventListener('mouseover', () => {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    const randomX = Math.floor(Math.random() * (windowWidth - button.clientWidth));
-    const randomY = Math.floor(Math.random() * (windowHeight - button.clientHeight));
+  button.addEventListener('mousemove', (event) => {
+    const buttonRect = button.getBoundingClientRect();
+    const offsetX = event.clientX - buttonRect.left;
+    const offsetY = event.clientY - buttonRect.top;
 
-    button.style.left = `${randomX}px`;
-    button.style.top = `${randomY}px`;
+    if (offsetX < 20) {
+      button.style.left = `${buttonRect.left + 20}px`;
+    } else if (offsetX > buttonRect.width - 20) {
+      button.style.left = `${buttonRect.left - 20}px`;
+    }
+
+    if (offsetY < 20) {
+      button.style.top = `${buttonRect.top + 20}px`;
+    } else if (offsetY > buttonRect.height - 20) {
+      button.style.top = `${buttonRect.top - 20}px`;
+    }
   });
 });
